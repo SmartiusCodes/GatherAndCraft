2faf491e7d3ff43e21c37f975a55d4c7537bc353
Smartius <marharstrand1@gmail.com>
2022-01-06 02:03:49 +0100

    register

--- Public/EnterGame.js
+++ Public/EnterGame.js
@@ -1,5 +1,15 @@
 var socket = io();
+var IGN;
 
 function EnterGame(username, password, method) {
 	socket.emit("EnterGame", username, password, method);
 }
+
+socket.on("EnterGameFailed", (reason) => {
+	alert(reason);
+});
+
+socket.on("EnterGameCompleted", (username) => {
+	IGN = username;
+	$("#content").load("Game.html");
+});
